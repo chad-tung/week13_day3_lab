088d5afe730beeb525fb56d9491c1cddc539f01b
chad-tung <chad.tung@gmail.com>
2017-12-13 15:23:11 +0000

    dunno

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -158,7 +158,44 @@ module.exports = CountryDropdownView;
 /* 2 */
 /***/ (function(module, exports) {
 
-throw new Error("Module parse failed: Unexpected token (14:25)\nYou may need an appropriate loader to handle this file type.\n|     var ul = document.getElementById('bucket-list')\n|     var li = document.createElement('li');\n|     var center = {country.latlng[0], country.latlng[1]}\n|     var map = document.createElement(\"main-map\")\n|     map = new MapWrapper(mapDiv, center, 2);");
+//
+// MapWrapper = require("/mapWrapper.js")
+
+var bucketListView = function(countries){
+  this.render(countries)
+};
+
+bucketListView.prototype = {
+  render: function(countries) {
+  countries.forEach(function(country){
+
+    var ul = document.getElementById('bucket-list')
+    var li = document.createElement('li');
+    // var center = {country.latlng[0], country.latlng[1]}
+    // var map = document.createElement("main-map")
+    // map = new MapWrapper(mapDiv, center, 2);
+    // mainMap.addMarker(center);
+    var details = document.createElement('p');
+      // details.innerText = country.name;
+      // details.appendChild(map);
+      li.appendChild(details);
+    var nameP = document.createElement('p');
+      nameP.innerText = country.name;
+      li.appendChild(nameP);
+    var capitalP = document.createElement('p');
+      capitalP.innerText = country.capital;
+      li.appendChild(capitalP);
+    var regionP= document.createElement('p');
+      regionP.innerText = country.region;
+      li.appendChild(regionP);
+      ul.appendChild(li);
+    })
+  }
+};
+
+
+module.exports = bucketListView;
+
 
 /***/ })
 /******/ ]);
